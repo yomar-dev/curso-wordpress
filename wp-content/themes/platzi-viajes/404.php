86da5b643c3b5b6416ad83bb44d590837a332cc2
Yomar Miranda <mirandayomar@gmail.com>
2018-01-28 22:17:39 -0500

    Cómo podemos ver hemos modificado la plantilla 404 sin afectar a la plantilla padre.

--- wp-content/themes/platzi-viajes/404.php
+++ wp-content/themes/platzi-viajes/404.php
@@ -16,10 +16,8 @@ get_header(); ?>
 				<div class="page-content">
 
 					<header class="page-header">
-						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'storefront' ); ?></h1>
+						<h1 class="page-title"><?php esc_html_e( '¡Vaya! La página no se encuentra.', 'storefront' ); ?></h1>
 					</header><!-- .page-header -->
-
-					<p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p>
 
 					<?php
 					echo '<section aria-label="' . esc_html__( 'Search', 'storefront' ) . '">';
