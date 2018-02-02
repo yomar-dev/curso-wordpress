8c780645d4673e4b2d66b2485195878f0cfa19a5
Yomar Miranda <mirandayomar@gmail.com>
2018-02-01 22:38:30 -0500

    Esta es la configuración básica que debe tener el plugin para poder ser reconocido y nos aparezca en nuestro panel de administración. Nota: Esta es la configuración básica que se utilizó en el curso, más adelante se puede cambiar.

---
+++ wp-content/plugins/platziplugin/platziplugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Platzi Plugin
+Plugin URI:  https://raven.es/plugins/platziplugin
+Description: Plugin con los elementos extra de Platzi
+Version:     1.0
+Author:      bi0xid
+Author URI:  https://raven.es/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: platziplugin
+Domain Path: /languages
+*/
